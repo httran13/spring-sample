2127231b7cb882e7574e7babadd6b2b7251aa34d
Huy Tran <httran8884@gmail.com>
2019-06-28 15:45:07 -0400

    add jacoco

---
+++ src/test/java/com/example/demo/TestMessageBuilder.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMessageBuilder {
+
+    @Test
+    public void testNameMkyong() {
+
+        MessageBuilder obj = new MessageBuilder();
+        assertEquals("Hello mkyong", obj.getMessage("mkyong"));
+
+    }
+
+}
